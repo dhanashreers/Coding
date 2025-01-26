6506e911d4ca628b1eacaa385e0ec0d9e60df121
dhanashreers <dhanashree.srinivasa@gmail.com>
2025-01-26 13:01:51 -0500

    Add files via upload

---
+++ log_file.py
@@ -0,0 +1,13 @@
+import csv
+
+def print_first_two_rows(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        # Loop through the first two rows and print them
+        for i, row in enumerate(reader):
+            print(row)
+            if i == 1:  # Stop after printing the first two rows
+                break
+
+# Call the function with your file
+print_first_two_rows('/Users/dhanashree/Downloads/logs.csv')
